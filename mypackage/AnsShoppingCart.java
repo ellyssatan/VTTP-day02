9de4cf3f38890cee3d8569a0fcab08870a49de9f
ellyssatan <ellyssa.tan1807@gmail.com>
2022-06-04 23:56:20 +0800

    updated

--- mypackage/AnsShoppingCart.java
+++ mypackage/AnsShoppingCart.java
@@ -12,31 +12,40 @@ public class AnsShoppingCart {
         boolean stop = false;
         String input;
         int delIndex = 0;
-        String item;
+        boolean exists = false;
 
+        System.out.println("Welcome to your shopping cart");
+        
         // main loop
         while (!stop) {
             input = cons.readLine("> ");
-
             String[] terms = input.split(" ");
+
+            // System.out.println(terms[0]);
 
             switch (terms[0]) {
                 case "add":
                     if (terms.length < 2) {
                         System.err.println("Please provide an item to add");
                     } else {
-                        for (int i = 1; i < cart.size(); i++) {
-                            item = terms[i];
-                            cart.add(item);
-                            System.out.printf("%s added to cart\n", item);
-
-                            for (int k = 0; k < cart.size(); k++) {
-                                if (item.equals(cart.get(k)))
-                                cart.remove(item);
-                                System.out.printf("You have %s in your cart\n", item);
+                        // loop through words in input
+                        for (int k = 1; k < terms.length; k++) {
+                            // assume not found
+                            exists = false;
+                            // loop through cart items
+                            for (int i = 0; i < cart.size(); i++) {
+                                if (terms[k].equals(cart.get(i))) {
+                                    exists = true;
+                                    System.out.printf("You have %s in your cart\n", terms[k]);
+                                    // drop out of loop once found
+                                    break;
+                                }
+                            }
+                            if (!exists) {
+                            cart.add(terms[k]);
+                            System.out.printf("%s added to cart\n", terms[k]);
                             }
                         }
-                        
                     }
                     break;
                 
@@ -56,13 +65,20 @@ public class AnsShoppingCart {
                     } else {
                         delIndex = Integer.parseInt(terms[1])-1;
                         if (delIndex < cart.size())
-                            cart.remove(delIndex);
                             System.out.printf("%s removed from cart\n", cart.get(delIndex));
+                            cart.remove(delIndex);
                     }
                     break;
                     
                 case "end":
+                    System.out.println("Thank you for shopping with us");
+                    
+                    stop = true;
                     break;
+                
+                default:
+                System.out.println("Please enter valid command");
+                    
 
                     
             }
